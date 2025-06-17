0b710adc7b18a2c122b8256863c800c891113267
Brot <sepp2017.huber@gmail.com>
2025-06-17 22:20:52 +0200

    The code is now able to dynamically extract the api key and print it to the console

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 #include <cstring>
+#include <fstream>
+#include <format>
 
 
 using namespace std;
@@ -22,14 +24,55 @@ void printVersion() {
     cout << "Please check back later for more features." << endl;
 }
 
+void printUnknownArgument(const char *arg) {
+    cout << "Unknown argument: " << arg << endl;
+    cout << "Use --help to see available options." << endl;
+}
+
+string getAPIKey() {
+    char * userpath = getenv("USERPROFILE");
+    string path;
+    if (userpath != nullptr)
+        path = std::string(userpath) + "\\.wakatime.cfg";
+    else
+        std::cout << "No user path";
+
+    fstream file(path,ios::in);
+    if (!file.is_open()) {
+        cout <<  format("Could not open the configuration file. Please ensure it exists at {}", path) << endl;
+        return "-1";
+    }
+    cout << "Reading configuration file from: " << path << endl;
+    for (string line; getline(file, line); ) {
+        if (line.find("api_key") != string::npos) {
+            size_t pos = line.find("=");
+            if (pos != string::npos) {
+                string api_key = line.substr(pos + 1);
+                return api_key;
+            }
+        }
+    }
+
+    return "-1";
+}
+
 void statsSystem() {
     cout << "Welcome to Hackatime Stats!" << endl;
     cout << "This program is designed to fetch and display your coding statistics from Hackatime." << endl;
     cout << "At the moment, it is a work in progress." << endl;
-    cout << "Please check back later for more features." << endl;
+    cout << "\n\n\n\n";
+
+
+    const string api_key = getAPIKey();
+    if (api_key == "-1") {
+        cout << "Exiting due to missing API key." << endl;
+        return;
+    }
+    cout << "Your API key is: " << api_key << endl;
+
 }
 
-int main( int argc, char *argv[]) {
+int main(const int argc, char *argv[]) {
     if (argc == 1) {
         statsSystem();
         return 0;
@@ -43,8 +86,7 @@ int main( int argc, char *argv[]) {
             cout << "Hackatime Stats version 1.0" << endl;
             return 0;
         }
-        cout << "Unknown argument: " << argv[1] << endl;
-        printHelp();
+        printUnknownArgument(argv[1]);
         return 1;
     }
     cout << "Too many arguments. Please use --help for more information." << endl;
